ff6d7f15d40dbac5a6f6d9c9559566118824f9d9
venutti <lvenutti@fi.uba.ar>
2022-12-23 17:16:27 -0300

    Agrega el componente Login
    Agrega validaciones y redireccionamientos.
    Usa el Token brindado por la API para validar ingresos a el resto de páginas.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Listado from "./components/Listado";
+import Login from "./components/Login";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/listado" element={<Listado />} />
+      </Routes>
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,83 @@
+import axios from "axios";
+import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
+
+const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+const validateEmail = (email) => email.match(emailRegex);
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.loginEmail.value;
+    const password = e.target.loginPassword.value;
+
+    // Validacion
+    if (!email || !password) {
+      swal({
+        title: "Campos vacíos",
+        text: "No puedes dejar campos en blanco",
+        icon: "error",
+      });
+      return;
+    }
+    if (!validateEmail(email)) {
+      swal({
+        title: "Email incorrecto",
+        text: "Debes ingresar un email de la forma example@example.com",
+        icon: "error",
+      });
+      return;
+    }
+    if (email !== "challenge@alkemy.org" || password !== "react") {
+      swal({
+        title: "Credenciales incorrectas",
+        text: "Los datos ingresados son incorrectos",
+        icon: "error",
+      });
+      return;
+    }
+
+    axios
+      .post("http://challenge-react.alkemy.org", { email, password })
+      .then((res) => {
+        // Persistencia del token
+        localStorage.setItem("token", res.data.token);
+        swal({
+          title: "Ingreso exitoso",
+          text: `¡Bienvenido ${email}!`,
+          icon: "success",
+        }).then(() => {
+          navigate("/listado");
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+        swal("Uups!", "Ocurrió un error con tus credenciales", "error");
+      });
+  };
+
+  return (
+    <Container className="login-form">
+      <h1>Iniciar sesión</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="loginEmail">
+          <Form.Label>Correo electrónico</Form.Label>
+          <Form.Control type="email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="loginPassword">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control type="password" />
+        </Form.Group>
+        <Button type="submit" variant="warning">
+          Ingresar
+        </Button>
+      </Form>
+    </Container>
+  );
+}
