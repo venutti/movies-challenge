7e7a6da8f17d3972641fcf1afcb07a65e4687c36
venutti <lvenutti@fi.uba.ar>
2023-01-09 14:21:28 -0300

    Solucion redireccionamiento del gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./custom.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/movies-challenge">
       <App />
     </BrowserRouter>
   </React.StrictMode>
