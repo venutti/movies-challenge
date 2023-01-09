665d4eb68c820b9f4fa684f22818c7bdfe8a1e77
venutti <lvenutti@fi.uba.ar>
2023-01-09 15:05:40 -0300

    Cambia BrowserRouter a HashRouter
    Tip encontrado en
    https://stackoverflow.com/questions/71984401/react-router-not-working-with-github-pages
    para arreglar el problema del homepage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./custom.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/movies-challenge">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
